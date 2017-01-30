be91b6e6b23837cd21b97f336fb10fda51a6bc6c
Priya2310 <cpd2393@gmail.com>
2017-01-30 18:17:47 +0530

    Add files via upload

---
+++ SampleForeach.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samplearray;
+
+/**
+ *
+ * @author Student
+ */
+public class SampleForeach {
+
+public void display()
+{
+int a[] = {11,45,32,67,49};
+    for (int i : a) 
+    {
+    System.out.println(i);
+    }
+}
+}
---
+++ TestForEach.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestSampleArray;
+
+import samplearray.SampleForeach;
+
+
+/**
+ *
+ * @author Student
+ */
+public class TestForEach 
+{
+    public static void main(String args[])
+    {
+    SampleForeach sf = new SampleForeach();
+    sf.display();
+    }
+}
