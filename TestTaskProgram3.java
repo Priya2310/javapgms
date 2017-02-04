1a581f9b4629cac3549761ce9cb81c40076b2941
Priya2310 <cpd2393@gmail.com>
2017-02-04 18:03:29 +0530

    Pyramid

---
+++ TestTaskProgram3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testcase;
+
+import javapgms.TaskProgram3;
+
+/**
+ *
+ * @author Student
+ */
+public class TestTaskProgram3 
+{
+public static void main(String args[])
+{
+    TaskProgram3 t = new TaskProgram3();
+    t.display();
+}
+}
